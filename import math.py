292abd1d6d612555ff96e44ac5b3f207f0384a72
vasDEV2 <vasudevanvr2002@gmail.com>
2025-03-03 13:55:18 +0100

    Cleaned up redundant files

--- import math.py
+++
@@ -1,8 +0,0 @@
-import math
-import time
-import numpy as np
-
-lat = np.load('/home/vasudevan/Desktop/editcode/arr_0.npy')
-lon = np.load('/home/vasudevan/Desktop/editcode/arr_1.npy')
-print(lat)
-print(lon)
